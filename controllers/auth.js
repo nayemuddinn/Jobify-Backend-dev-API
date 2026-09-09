58d57d849dfd22bc21c3c5e4a3d448be35c6272c
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 12:39:50 +0600

    feat(auth Registration): Implemented the user registration functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,11 @@ const login = async (req, res) => {
 }
 
 const registration = async (req, res) => {
-
+    const user = await User.create({ ...req.body })
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({
+        user: { name: user.name }, token
+    })
 }
 
 module.exports = { login, registration }
