370186e1ec7dc49efa8e9bbc7dad33cd05e6072b
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 16:59:17 +0600

    feat(jobController):implemented createJob and getAlljob operation

--- controllers/job.js
+++ controllers/job.js
@@ -4,14 +4,17 @@ const { BadRequestError, NotFoundError } = require('../errors')
 
 
 const getAllJobs = async (req, res) => {
-
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 const getJob = async (req, res) => {
 
 }
 
 const createJob = async (req, res) => {
-
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
