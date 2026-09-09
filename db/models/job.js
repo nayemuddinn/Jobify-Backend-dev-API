7bdef8413b83be5dd2c051dcc5d8a8f7b02e678b
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 11:35:53 +0600

    feat(auth): add login and registration routes

---
+++ db/models/job.js
@@ -0,0 +1,33 @@
+const mongoose = require('./mongoose')
+
+const jobSchema = new mongoose.Schema(
+    {
+        company: {
+            type: String,
+            required: [true, 'Please provide a company name'],
+            maxlength: 20
+        },
+        position: {
+            type: String,
+            required: [true, 'Please provide position'],
+            maxlength: 100
+        },
+        status: {
+            type: String,
+            enum: ['interview', 'declined', 'pending'],
+            default: 'pending'
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide user']
+        }
+
+
+    },
+    {
+        timestamp: true
+    }
+)
+
+module.exports = mongoose.model('Jobs', jobSchema)
---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router();
+
+const { login, registration } = require('../controllers/auth')
+
+router.post('/login', login)
+router.post('/registration', registration)
