e6a45bfe02542cfc5b3582f87626f708764738ae
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 16:00:14 +0600

    fix(authMiddleware):added error handler classes

--- middlware/auth/authMiddleware.js
+++ middlware/auth/authMiddleware.js
@@ -1,12 +1,13 @@
 const User = require('../../db/models/user')
 const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
 
 
 const auth = async (req, res, next) => {
     const authHeader = req.headers.authorization
 
     if (!authHeader || authHeader.startsWith('Beared'))
-        ;;//will throw error
+        throw new UnauthenticatedError('Authentication invalid')
     const token = authHeader.split(' ')[1]
 
     try {
@@ -18,6 +19,8 @@ const auth = async (req, res, next) => {
 
     }
     catch (error) {
-        ;//will throw error
+        throw new UnauthenticatedError('Authentication invalid')
     }
 }
+
+module.exports = auth
