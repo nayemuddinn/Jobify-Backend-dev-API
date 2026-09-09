fd549aa7af69b5921704b65c44bec27fa79a9e22
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 12:58:26 +0600

    feat(auth): Json web token verification middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@ const User = require('../db/models/user')
 const { StatusCodes } = require('http-status-codes')
 
 const login = async (req, res) => {
-
     res.status(StatusCodes.OK).json({ name: 'OK' })
 }
 
