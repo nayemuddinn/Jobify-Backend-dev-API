305624308fcaa1269630b90e9ee66e0d05a760fd
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 17:35:50 +0600

    feat(jobController):Implemented delete operaton function

--- controllers/job.js
+++ controllers/job.js
@@ -36,7 +36,17 @@ const updateJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
+    const {
+        user: { userId },
+        params: { id: jobId }
+    } = req;
 
+    const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
