61bb91a86823006d81108e936ed41be5a017afd0
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 17:27:52 +0600

    feat(jobController):Added create job functionality

--- controllers/job.js
+++ controllers/job.js
@@ -8,7 +8,20 @@ const getAllJobs = async (req, res) => {
     res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 const getJob = async (req, res) => {
+    const {
+        user: { userId },
+        params: { id: jobId }
+    } = req;
 
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
