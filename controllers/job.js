cf30e4eb5b130305e71f45dab5c22d9c8327521e
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 16:33:57 +0600

    feat(jobController):Added the Crud operation functions

---
+++ controllers/job.js
@@ -0,0 +1,32 @@
+const Job = require('../db/models/job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+
+const getAllJobs = async (req, res) => {
+
+}
+const getJob = async (req, res) => {
+
+}
+
+const createJob = async (req, res) => {
+
+}
+
+const updateJob = async (req, res) => {
+
+
+}
+
+const deleteJob = async (req, res) => {
+
+}
+
+module.exports = {
+    createJob,
+    deleteJob,
+    getAllJobs,
+    updateJob,
+    getJob,
+}
--- routes/job.js
+++ routes/job.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-// const { createJob,
-//     deleteJob,
-//     getAllJobs,
-//     updateJob,
-//     getJob, } = require('../controllers/job')
+const { createJob,
+    deleteJob,
+    getAllJobs,
+    updateJob,
+    getJob, } = require('../controllers/job')
 
-// router.route('/').post(createJob).get(getAllJobs)
-// router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.route('/').post(createJob).get(getAllJobs)
+router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
 
 module.exports = router
