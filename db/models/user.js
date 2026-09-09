3bb3d57d3eca7cd121b94cce49b99ef42fd4fafb
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-09 11:13:36 +0600

    feat(db): created user and job model for mongodb

---
+++ db/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('./mongoose')
+
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: ['true,Please provide a name'],
+            maxlength: 50,
+            minLength: 3
+        },
+
+        email: {
+            type: String,
+            required: [true, 'Please provide email'],
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                'Please provide a valid email',
+            ],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Please provide password'],
+            minlength: 6,
+        },
+    }
+)
+
+module.exports = mongoose.model('User', userSchema)
